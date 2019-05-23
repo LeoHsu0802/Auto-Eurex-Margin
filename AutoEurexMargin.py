494c41292e09f5b070e860d07ee94bc7112b0739
LeoHsu0802 <38006639+LeoHsu0802@users.noreply.github.com>
2019-05-23 11:26:15 +0800

    Update AutoEurexMargin.py

--- AutoEurexMargin.py
+++ AutoEurexMargin.py
@@ -4,18 +4,20 @@ import os
 import time
 import requests
 
+#創建日期
 date=time.strftime("%M%S%H%d%m%y")
 date_for_file=time.strftime("%Y%m%d%H")
-
+#存檔路徑
 path=r"EurexMargin\\"+time.strftime("%y%m")+"\\"
-
+#若無該路徑，則創建一個
 if not os.path.isdir(r"EurexMargin\\"):
 	os.mkdir(r"EurexMargin\\")
 
 if not os.path.isdir(path):
 	os.mkdir(path)
-
+#檔名為 Eurex Margin_" 今日 年月日
 file_name=("Eurex Margin_"+time.strftime("%Y")+time.strftime("%m")+time.strftime("%d")+".xls")
+#取得檔案
 dls = "http://www.eurexclearing.com/blob/2535516/f12a6596be2256ffcd3a14bd0a5d9f21/data/MarginParametersEstimationCircular.xls"
 resp = requests.get(dls)
 output = open(path+file_name, 'wb')
